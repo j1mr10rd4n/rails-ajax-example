142c96a8ed3ae5ba7d3df18ae98289df9f36886f
Jim Riordan <jim@j1mr10rd4n.info>
2014-10-20 14:02:09 +1100

    Widget requires values for choice_one and choice_two

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,7 +1,13 @@
 class Widget < ActiveRecord::Base
 
+  validates_presence_of :choice_one, :choice_two
+
   def choice_one_possibilities
     %w{foo bar baz qux}
+  end
+
+  def choice_two_possibilities
+    []
   end
 
 end
